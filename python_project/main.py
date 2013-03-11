aeaf41e1c0b60833dbbf0a39f04333b96b074e14
paulmayer <equilibrium87@gmail.com>
2013-03-11 08:02:02 +0100

    prelim partitioning

--- python_project/main.py
+++ python_project/main.py
@@ -3,9 +3,10 @@ from solver import LapackBenchmark
 #from spyke import Spyke
 import numpy as np
 
-A = sparse_creator.create_banded_matrix(100000,20,20)
-x = np.ones(100000)
+A = sparse_creator.create_banded_matrix(20000,100,100)
+x = np.ones(20000)
 b = sparse_creator.create_rhs(A, x)
 
+print type(A.data)
 lapack_sparse = LapackBenchmark(A,b,x)
 lapack_sparse.benchmark_spsolve()
--- python_project/spyke/spyke.py
+++ python_project/spyke/spyke.py
@@ -1,7 +1,6 @@
 import pyopencl as cl
 import numpy as np
 import scipy as sp
-from utils import sparse_creator
 
 class SpykeSolver(object):
 	'''
@@ -9,9 +8,23 @@ class SpykeSolver(object):
 	Initializes elements to solve Ax = b.
 	x and b are assument column vectors
 	'''
-	def __init__(self, matrix, rhs): 
-		pass
+	def __init__(self, A, b, x): 
+		self.A = A
+		self.b = b
+		self.x = x
 
+	def __ds_factor(self, A, b, p):
+		'''
+		DS factorization of A and computation of
+		new rhs.
+		@param A: A sparse banded matrix (CSR).
+		@param b: RHS of Ax = b, nx1 numpy array.
+		@param p: The number of matrix partitions.
+		'''
+		if not p % 2:
+			raise ValueError('p has to be a power of 2')
+ 
+	
 	def recursive_spike(self):
 		pass
 
