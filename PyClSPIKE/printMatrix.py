f95b5d2db0eff72d775ce37c96c0af3d0e99ff69
Natanael Arndt <arndtn@gmail.com>
2013-03-14 22:30:11 +0100

    allow m and n parameters for printMatrix

--- PyClSPIKE/printMatrix.py
+++ PyClSPIKE/printMatrix.py
@@ -1,16 +1,12 @@
 import numpy
 
-def printMatrix(config, queue, program, A_buf):
+def printMatrix(config, queue, program, A_buf, m = 0, n = 0):
     kernel = program.printMatrix
     kernel.set_scalar_arg_dtypes([None, numpy.int32, numpy.int32])
 
-    # I hope it also takes the last column
-    # how can I put only k jobs into the queue
-    kernel(
-        queue,
-        (1,),
-        None,
-        A_buf,  # A matrix
-        numpy.int32(config['matrixSize']),
-        numpy.int32(config['partitionSize'] + 2 * config['offdiagonalSize'] + config['rhsSize'])
-    )
+    if (m == 0) :
+        m = numpy.int32(config['matrixSize'])
+    if (n == 0) :
+        n = numpy.int32(config['partitionSize'] + 2 * config['offdiagonalSize'] + config['rhsSize'])
+
+    kernel(queue, (1,), None, A_buf, m, n)
